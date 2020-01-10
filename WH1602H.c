6b35c814691c7e138158b20f8707fe55f2550e37
sgtkingo <33989823+sgtkingo@users.noreply.github.com>
2020-01-10 22:37:22 +0100

    upd100120
    Final, WORK!!!

--- WH1602H.c
+++ WH1602H.c
@@ -5,30 +5,25 @@
  * Created on 3. listopadu 2019, 15:21
  */
 #include "PICF18LF46K22_ConfigSFR.h"
-#include "LCD_Engine.h"
+#include "LCD_Engine4bit.h"
 
 void InitDevice(){
-    OSCCON=0b01111100; //osc setting, 16 MHz, internal by FOSCH  
-    ANSELD=0x00;
-    TRISD=0x00;
-    
+    OSCCON=0b01111100; //osc setting, 16 MHz, internal by FOSCH     
     LCD_Initialize();
     __delay_ms(10);
 }
 
 void ClearDevice(){
-    PORTD=0x00;
     LCD_Clear();
-    LCDGoto(0,0);
 }
 void TestDevice(){
-    LCDPutStr("Hello Word");
+    LCDPutStr("Vono to zije!");
     __delay_ms(1000);
 }
 
 void main(void) {
     InitDevice();
-    ClearDevice();
+    //ClearDevice();
     TestDevice();
     __delay_ms(100);
     while(1);
